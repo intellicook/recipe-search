ad547440071b68eb0f12332f567fa295afcc44b7
LioQing <s2010157@gmail.com>
2024-10-01 12:00:14 +0800

    Add get health test

--- configs/base.py
+++ configs/base.py
@@ -1,10 +1,16 @@
+import os
 from typing import Tuple, Type
 
 from pydantic_settings import BaseSettings, PydanticBaseSettingsSource
 
 
-class BaseConfigs(BaseSettings):
+class BaseConfigs(BaseSettings, env_parse_none_str="None"):
     """Base configuration"""
+
+    def __init__(self, **kwargs):
+        if os.getenv("_TESTING"):
+            kwargs["_env_file"] = ".env.test"
+        super().__init__(**kwargs)
 
     @classmethod
     def settings_customise_sources(
