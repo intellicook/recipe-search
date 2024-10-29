0e391b6462c6fd307ba25a4d3324b363ef66fcad
LioQing <s2010157@gmail.com>
2024-10-29 14:26:21 +0800

    Add get recipe not found handling

--- tests/test_get_recipe.py
+++ tests/test_get_recipe.py
@@ -1,4 +1,7 @@
+import grpc
+import pytest
 import pytest_mock
+from sqlalchemy.exc import NoResultFound
 
 from apis.servicer import RecipeSearchServicer
 from infra import models
@@ -33,4 +36,31 @@ def test_get_recipe_success(
     )
     assert all(
         x == y for x, y in zip(response.instructions, recipe.instructions)
+    )
+
+
+def test_get_recipe_not_found(
+    mocker: pytest_mock.MockerFixture,
+):
+    id = 1
+    request = RecipeRequest(
+        id=id,
+    )
+
+    mock_get_recipe = mocker.patch(
+        "domain.controllers.get_recipe",
+        side_effect=NoResultFound(),
+    )
+
+    context = mocker.MagicMock()
+    context.abort = mocker.MagicMock(side_effect=grpc.RpcError)
+
+    servicer = RecipeSearchServicer()
+    with pytest.raises(grpc.RpcError):
+        servicer.GetRecipe(request, context)
+
+    mock_get_recipe.assert_called_once_with(id)
+    context.abort.assert_called_once_with(
+        grpc.StatusCode.NOT_FOUND,
+        f"Recipe with ID {id} not found",
     )
