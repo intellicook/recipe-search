12805bd3970eab92c6df1bd826387f3b451a92e8
LioQing <s2010157@gmail.com>
2024-11-10 14:35:18 +0800

    Add chat model of GPT4o mini

--- domain/chats/__init__.py
+++ domain/chats/__init__.py
@@ -3,9 +3,11 @@ from typing import Dict, Type
 from configs.domain import configs
 from domain.chats.base import BaseChat
 from domain.chats.gpt4o import GPT4OChat
+from domain.chats.gpt4o_mini import GPT4OMiniChat
 
 mapping: Dict[str, Type[BaseChat]] = {
     "gpt4o": GPT4OChat,
+    "gpt4o_mini": GPT4OMiniChat,
 }
 
 model = mapping[configs.chat_model]
